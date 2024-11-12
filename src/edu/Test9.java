dabed6ef35485b9c6433d226af22658f3034ce03
박영하 (Youngha Park) <youngha0930@outlook.com>
2024-11-12 12:27:19 +0900

    Test9.java

---
+++ src/edu/Test9.java
@@ -0,0 +1,19 @@
+package edu;
+
+public class Test9 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+        a += b;
+        System.out.println(a);
+        a -= b;
+        System.out.println(a);
+        a *= b;
+        System.out.println(a);
+        a /= b;
+        System.out.println(a);
+        a %= b;
+        System.out.println(a);
+    }
+}
