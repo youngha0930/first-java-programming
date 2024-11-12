7ac4f0388dee99d5d7fd0e3c2f5b6b55567c1bc6
박영하 (Youngha Park) <youngha0930@outlook.com>
2024-11-12 20:47:39 +0900

    src/edu/Test26.java

---
+++ src/edu/Test26.java
@@ -0,0 +1,12 @@
+package edu;
+
+public class Test26 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("OK");
+    }
+}
