73e08764e96e782891e3ea7efcbddb2563aa3879
박영하 (Youngha Park) <youngha0930@outlook.com>
2024-11-12 11:36:44 +0900

    Test8.java

---
+++ src/edu/Test8.java
@@ -0,0 +1,15 @@
+package edu;
+
+public class Test8 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
